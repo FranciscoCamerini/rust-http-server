5329698cf67df7148e82d50efe191ba4997b087b
Francisco Camerini <franciscocamerini@onsign.tv>
2023-02-24 21:53:28 -0300

    Initialize repository

---
+++ target/debug/deps/http_server-af240c9b7913a8bb.d
@@ -0,0 +1,5 @@
+/home/onsigntv/Dev/rust/http-server/target/debug/deps/http_server-af240c9b7913a8bb: src/main.rs
+
+/home/onsigntv/Dev/rust/http-server/target/debug/deps/http_server-af240c9b7913a8bb.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/http-server.d
@@ -0,0 +1,1 @@
+/home/onsigntv/Dev/rust/http-server/target/debug/http-server: /home/onsigntv/Dev/rust/http-server/src/main.rs
