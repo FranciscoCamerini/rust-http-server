bbd2093f6026f4ffffb8520e476be0da02c7852f
Francisco Camerini <franciscocamerini@onsign.tv>
2023-03-05 19:31:53 -0300

    Create base module structure

---
+++ src/http/request.rs
@@ -0,0 +1,5 @@
+pub struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: super::Method,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+mod http;
+mod server;
+
+use http::Request;
+use http::Method;
+use server::Server;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
